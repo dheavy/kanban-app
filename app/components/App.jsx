1c168b7191805c4cae5df65ed29ba46305e25bd1
Davy Peter Braun <hello@davybraun.com>
2015-11-09 10:33:53 +0100

    Stub Lanes.

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,37 +1,28 @@
 import uuid from 'node-uuid';
 import React, {Component} from 'react';
 import AltContainer from 'alt-container';
-import Notes from './Notes.jsx';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import Lanes from './Lanes.jsx';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends Component {
+  // The AltContainer allows us to bind data to its immediate children.
   render() {
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-
-        // The AltContainer allows us to bind data to its immediate
-        // children. In this case it injects the `items` property into
-        // `Notes`.
-        <AltContainer stores={[NoteStore]} inject={{
-          items: () => NoteStore.getState().notes
-        }}>
-          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        <button className="add-lane" onClick={this.addItem}>+</button>
+        <AltContainer
+          stores={[LaneStore]}
+          inject={{
+            items: () => LaneStore.getState().notes
+          }}>
+          <Lanes onEdit={this.editLane} onDelete={this.deleteLane} />
         </AltContainer>
       </div>
     );
   }
 
-  addNote() {
-    NoteActions.create({task: 'New task'});
-  }
-
-  editNote(id, task) {
-    NoteActions.update({id, task});
-  }
-
-  deleteNote(id) {
-    NoteActions.delete(id);
+  addItem() {
+    LaneActions.create({task: 'New lane'});
   }
 }
